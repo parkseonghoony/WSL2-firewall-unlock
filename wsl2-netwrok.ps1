faf3711750f6af0591d076ebf9d79b45cc19877a
Park, Seonghoon <park.s@yonsei.ac.kr>
2020-12-21 10:13:28 +0900

    Create wsl2-netwrok.ps1

---
+++ wsl2-netwrok.ps1
@@ -0,0 +1,38 @@
+$remoteport = bash.exe -c "ifconfig eth0 | grep 'inet '"
+$found = $remoteport -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
+
+if( $found ){
+    $remoteport = $matches[0];
+} else{
+    echo "The Script Exited, the ip address of WSL 2 cannot be found";
+    exit;
+}
+
+#[Ports]
+
+#All the ports you want to forward separated by coma
+# 이 부분만 수정하면 되는 듯
+$ports=@(80, 443, 8888, 11080);
+
+
+#[Static ip]
+#You can change the addr to your ip config to listen to a specific address
+$addr='0.0.0.0';
+$ports_a = $ports -join ",";
+
+
+#Remove Firewall Exception Rules
+iex "Remove-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' ";
+
+#adding Exception Rules for inbound and outbound Rules
+iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort $ports_a -Action Allow -Protocol TCP";
+iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -LocalPort $ports_a -Action Allow -Protocol TCP";
+
+for( $i = 0; $i -lt $ports.length; $i++ ){
+    $port = $ports[$i];
+    iex "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$addr";
+    iex "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$addr connectport=$port connectaddress=$remoteport";
+}
+
+# Execution policy 관련 오류 발생하면
+# PowerShell.exe -ExecutionPolicy Bypass -File .\wsl-forward-server.ps1
